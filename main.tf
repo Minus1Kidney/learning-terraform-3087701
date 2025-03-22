00f8febb54e5f7d9eb9562a42f05be97b21d99ac
Minus1Kidney <Raph@Minus1Kidney.com>
2025-03-22 21:59:12 +0000

    More typo corrections

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_vps" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
@@ -30,7 +30,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
